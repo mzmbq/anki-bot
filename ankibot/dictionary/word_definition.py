bc3d77fdfa3459b3a480ecbb57ddb45e3b6db006
mzmbq <100804739+mzmbq@users.noreply.github.com>
2023-02-16 17:04:34 +0100

    Add __str__ to WordDefinition

--- ankibot/dictionary/word_definition.py
+++ ankibot/dictionary/word_definition.py
@@ -5,3 +5,7 @@ class WordDefinition:
     word: str
     definition: str
     examples: list[str]
+    
+    def __str__(self) -> str:
+        examples_list = "\n".join(["- " + e for e in self.examples])
+        return f"{self.word}\n{self.definition}\n{examples_list}"
