64d5d1fe123e911ae44ab43d511ad52e55a48431
mzmbq <100804739+mzmbq@users.noreply.github.com>
2023-08-31 20:35:44 +0200

    Make Dictionary class abstract

--- ankibot/dictionary/dictionary.py
+++ ankibot/dictionary/dictionary.py
@@ -1,18 +1,21 @@
 from ankibot.dictionary.word_definition import WordDefinition
 
+from abc import ABC, abstractmethod
 
-class Dictionary:
+
+class Dictionary(ABC):
+    @abstractmethod
     def __init__(self):
         pass
 
+    @abstractmethod
     def __contains__(self, word: str) -> bool:
         return False
 
+    @abstractmethod
     def get_definitions(self, word: str) -> list[WordDefinition]:
         return []
 
 
 class DictionaryError(Exception):
     pass
-
-# CambridgeDictionary().contains("fixture")
